abf9a9fd4c934a1d37b0e2cd654f96db3501afb1
yurim0525 <lovekook5813@gmail.com>
2025-09-14 04:50:24 +0900

    서브 메뉴

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject menuSet;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sub Menu
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuSet.activeSelf)
+                menuSet.SetActive(false);
+            else
+                menuSet.SetActive(true);
+        }
+    }
+
+    public void GameExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
